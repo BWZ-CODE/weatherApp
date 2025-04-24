848696e3879c52e0c9f6951dca83090300587db8
BWZ-CODE <mmige24@gmail.com>
2025-04-24 16:57:00 -0500

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Miguel Angel Bohorquez on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
